6e6ecfc86cc1821f838b0a9b8076af750fa9e88e
Adam Belin <adambelin1992@gmail.com>
2022-03-21 22:11:44 -0400

    added api and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
+
 function tempConversion(temp) {
-    return (temp - 273.15) * 9/5 + 32 
+    return Math.floor((temp - 273.15) * 9/5 + 32 );
 }
 
 function getWeather () {
@@ -8,20 +9,34 @@ function getWeather () {
     console.log(searchText);
 
     let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchText + "&appid=b1ceae313f004765b6fc393e0cc38135";
-        
-    fetch(apiUrl)
-    .then(response => response.json())
-    .then(function (response) {
-      
-        let temp = tempConversion(response.main.temp);
 
-       console.log(response.main)
-       console.log(temp);
-    })
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + searchText + "&limit=2&appid=b1ceae313f004765b6fc393e0cc38135")
+
+        .then(response => response.json())
+        .then(function (response) {
+
+            let long = response[0].lon 
+            let lat = response[0].lat
+
+            fetch("http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&appid=b1ceae313f004765b6fc393e0cc38135")
+                .then(response => response.json())
+                .then(function (response) {
+      
+                let temp = tempConversion(response.current.temp);
+
+            console.log(temp);
+         })
+    })     
 }; 
 
 $(document).ready(function () {
 
     $("#searchBtn").on("click", getWeather);
-
+    $("#search").keypress(function(event) {
+        let keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode === 13) {
+            getWeather();
+            $("#search").val("");
+        }
+    })
 });
